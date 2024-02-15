4b8b481fd614a0a22d2d79bc8ab8d3343c9262c6
Serges YAMAKO <yamakoserges@gmail.com>
2024-02-15 16:02:48 +0100

    Back end spring boot and font end angular

---
+++ Spring-Secuirty-Jwt-In-Spring-Boot-3/src/main/java/com/springjwt/services/auth/AuthService.java
@@ -0,0 +1,9 @@
+package com.springjwt.services.auth;
+
+import com.springjwt.dto.SignupDTO;
+import com.springjwt.dto.UserDTO;
+
+public interface AuthService {
+    UserDTO createUser(SignupDTO signupDTO);
+
+}
